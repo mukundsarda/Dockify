5e89fb50b8abab33d7746c6bdafe5875dc0773f6
Mukund Sarda <92849613+mukundsarda@users.noreply.github.com>
2024-07-02 18:15:42 +0530

    Add files via upload

---
+++ 2048/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { Button } from "./components/Button";
+import { Game } from "./components/Game";
+
+import "./App.less";
+
+/* eslint-disable react/jsx-no-target-blank */
+export const App = () => {
+  const [date, setDate] = useState<Date>(new Date());
+
+  const handleRestart = () => {
+    setDate(new Date());
+  };
+
+  return (
+    <div className="App">
+      <div className="header">
+        <div>
+          <h1>Play 2048</h1>
+        </div>
+        <div>
+          <Button onClick={handleRestart}>Restart</Button>
+        </div>
+      </div>
+      <Game key={date.toISOString()} />
+    </div>
+  );
+};
+/* eslint-enable react/jsx-no-target-blank */
